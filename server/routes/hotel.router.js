ff4d5b236c641dc7be64c0efcb22108d631383d8
josedavidnup <josedavidnup@gmail.com>
2023-03-08 11:26:40 -0400

    error fixed with amadeus

--- server/routes/hotel.router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getHotelByCity } = require("../services/hotel.service");
-const router = express.Router();
-
-router.get("/hotel-by-city", getHotelByCity);
-
-module.exports = router;
