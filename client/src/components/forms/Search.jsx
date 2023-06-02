eb18646a1517f9ac6e9a57603e2621923afcac36
josedavidnup <josedavidnup@gmail.com>
2023-06-02 12:17:20 -0400

    new design for searching room

--- client/src/components/forms/Search.jsx
+++ client/src/components/forms/Search.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
-import { DatePicker, Select } from "antd";
-import { AiOutlineSearch } from "react-icons/ai";
-import moment from "moment";
-import ReactGoogleAutocomplete from "react-google-autocomplete";
+import React, { useState } from 'react';
+import { DatePicker, Select } from 'antd';
+import { AiOutlineSearch } from 'react-icons/ai';
+import moment from 'moment';
+import ReactGoogleAutocomplete from 'react-google-autocomplete';
 const config = import.meta.env.VITE_GOOGLEPLACES_API_KEY;
 const { RangePicker } = DatePicker;
 const { Option } = Select;
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
-  const [location, setLocation] = useState("");
-  const [date, setDate] = useState("");
-  const [bed, setBed] = useState("");
+  const [location, setLocation] = useState('');
+  const [date, setDate] = useState('');
+  const [bed, setBed] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = () => {
@@ -21,25 +21,25 @@ const Search = () => {
   return (
     <div>
       <ReactGoogleAutocomplete
-        className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-        placeholder="Location"
+        className='block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 h-12 px-4'
+        placeholder='Location'
         apiKey={config}
         defaultValue={location}
         onPlaceSelected={(place) => {
           setLocation(place.formatted_address);
         }}
-        style={{ height: "50px" }}
       />
       <RangePicker
         onChange={(value, dateString) => setDate(dateString)}
         disabledDate={(current) =>
-          current && current.valueOf() < moment().subtract(1, "days")
+          current && current.valueOf() < moment().subtract(1, 'days')
         }
+        className='mt-4'
       />
       <Select
         onChange={(value) => setBed(value)}
-        placeholder="Number of beds"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder='Number of beds'
+        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
       >
         <Option key={1}>1</Option>
         <Option key={2}>2</Option>
@@ -48,7 +48,7 @@ const Search = () => {
       </Select>
       <AiOutlineSearch
         onClick={handleSubmit}
-        className="inline-flex items-center cursor-pointer px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+        className='inline-flex items-center cursor-pointer px-5 py-2.5 mt-4 text-sm font-medium text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800'
       />
     </div>
   );
